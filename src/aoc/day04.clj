5055b67261703d4c758a4a23c46aa6f4981e9914
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2023-12-06 20:27:47 +0100

    Day 4 is not over, but I'm quite sure there is an interesting idea behind

--- src/aoc/day04.clj
+++ src/aoc/day04.clj
@@ -5,9 +5,15 @@
             [clojure.math :as math]))
 
 (comment
-  (parse-line
-   "Card   1: 98 16 95 90 53 33 43  7 46 45 | 85 15 78 57 34 10 46 90 33 13  8 54  4 37 25 63 55 41  7 82 69 16 30 76  2"
-   )
+  (def cards {
+              :1 (parse-line "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+  })
+   (count-all-winning-cards "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+  Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+  Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+  Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+  Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+  Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
   )
 
 (defn
@@ -19,21 +25,20 @@
         winning (set (filter (fn [text] (> (count text)0)) (str/split (str/trim winning-text) #" ")))
         my (set (filter (fn [text] (> (count text) 0))  (str/split (str/trim my-text) #" ")))
         ]
-    {:index line-index :winning winning :my my}
+    {:index line-index :winning winning :my my :line input}
     )
   )
 
 (defn
-  compute-score-of-line
-  [input]
-  (let [detail (parse-line input)
+  compute-score-of-card
+  [detail]
+  (let [
         inter (aset/intersection (get detail :winning) (get detail :my))
         matched-count (count inter)
         score (if (= matched-count 0)
-                 0
-                 (int (math/pow 2 (- matched-count 1))))
+                0
+                (int (math/pow 2 (- matched-count 1))))
         ]
-;    (println (format "%s => %s" (get detail :index) score))
     score
   )
   )
@@ -42,9 +47,37 @@
   sum-scores-of-winning-cards
   [input]
   (let [lines (str/split-lines input)
-        scores (map compute-score-of-line lines)
+        cards (map parse-line lines)
+        scores (map compute-score-of-card cards)
         ]
     (reduce + scores)))
+
+(defn extend-with-score
+  [c]
+  (let [score {:score (compute-score-of-card c)}]
+    (merge c score)
+    )
+)
+(defn repeat-winning-cards-in
+  [input-list index]
+  (if (= index (count input-list))
+    input-list
+    (let [examined (nth input-list index)
+          size-of-won-cards (min (get examined :score)(- (count input-list) index))
+          won-cards (take size-of-won-cards input-list)
+          updated-list (concat input-list won-cards)]
+      (repeat-winning-cards-in updated-list (inc index)))
+    ))
+(defn
+  count-all-winning-cards
+  [input]
+  (let [lines (str/split-lines input)
+        cards (map parse-line lines)
+        scored-cards (map extend-with-score cards)
+        repeated-cards (repeat-winning-cards-in scored-cards 0)
+        ]
+    repeated-cards
+    ))
 
 (def text (slurp (.getFile (io/resource "day04.txt"))))
 (sum-scores-of-winning-cards text)
